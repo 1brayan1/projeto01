994d18589b8b9801af268879a8993a2e091bca99
brayan.waltrick <brayan.waltrick@PLE111D22.alunos.sc.senac.br>
2021-08-02 21:56:38 -0300

    Projeto da faculdade Senac, aprendendo lógica com Programação Orientada a Objeto POO.02/08/2021 Testando envio.02/08/2021 Gerador CPF e número aleatório. 02/08/2021

--- src/util/GeradorUtil.java
+++ src/util/GeradorUtil.java
@@ -10,15 +10,34 @@ package util;
  * @author brayan.waltrick
  */
 public class GeradorUtil {
+
+    /**
+     * Retorna um valor String conforme a quantidade de número solicitado
+     *
+     */
+    public String gerarNumero(int qtde) {
+        String senha = "";
+        int numero;
+        for(int i = 0; i < qtde; i++) {
+            numero = (int)(Math.random() * 10);
+            senha = senha + numero;
+        }
+        return senha;
+    }
+
+    public String gerarCpf(){
+        return gerarNumero(3) + "." + gerarNumero(3) + "." + gerarNumero(3) + "-" 
+                + gerarNumero(2);
+    }
     
-    public double gerarSalario(){
-        int numero = (int)(Math.random() * 10000);
+    public double gerarSalario() {
+        int numero = (int) (Math.random() * 10000);
         return numero;
     }
-    
+
     public static void main(String[] args) {
-        GeradorUtil objetoDessaClasse = new GeradorUtil();
-        System.out.println("Salario: " + objetoDessaClasse.gerarSalario());
+        GeradorUtil util = new GeradorUtil();
+        String cpf = util.gerarCpf();
+        System.out.println("CPF:" + cpf);
     }
-}    
-   
+}
